01606bac8b2184a6b0a7f018973deca92af12562
abdollah-mohajeri <miladeditor@gmail.com>
2025-11-24 13:51:57 +0330

    feat: add CoreSupply.APIGateway service to the microservices architecture

---
+++ CoreSupply.APIGateway/Program.cs
@@ -0,0 +1,29 @@
+﻿using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
+using Microsoft.AspNetCore.Hosting;
+
+var builder = WebApplication.CreateBuilder(args);
+
+
+builder.Configuration.AddJsonFile("ocelot.json", optional: false, reloadOnChange: true);
+
+
+builder.Services.AddOcelot();
+
+
+builder.Services.AddControllers();
+
+var app = builder.Build();
+
+
+if (app.Environment.IsDevelopment())
+{
+  
+    app.MapGet("/", () => "CoreSupply API Gateway is running in Development Mode.");
+}
+
+
+await app.UseOcelot();
+
+
+app.Run();
